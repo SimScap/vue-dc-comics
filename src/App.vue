27eb1fbacc7b7602b4aaeff55ca43ad46ccc238f
SimoneScapin <simonescap87@gmail.com>
2022-03-28 19:53:55 +0200

    aggiunto main ma problemi con l immagine e le liste

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
     <Header/>
     <Banner/>
+    <Main/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import Banner from './components/Banner.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Banner
+    Banner,
+    Main,
+    Footer 
   }
 }
 </script>
